4dd85a985077e0ed729655c9e161983e9764ae3c
Cameron Lloyd <lloyd@med.unideb.hu>
2019-03-20 21:11:14 +0100

    Update license header dates

--- src/main/java/hu/unideb/med/biophys/RiHelpWindow.java
+++ src/main/java/hu/unideb/med/biophys/RiHelpWindow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 - 2018 RiFRET developers.
+ * Copyright (C) 2009 - 2019 RiFRET developers.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
