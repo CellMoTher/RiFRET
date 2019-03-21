8fcb6d74665efd8dc79e9121e7633fdccdde8697
Cameron Lloyd <lloyd@med.unideb.hu>
2019-03-21 14:16:29 +0100

    Feat: add reset threshold to S factor windows

--- src/main/java/hu/unideb/med/biophys/S1S3Dialog.java
+++ src/main/java/hu/unideb/med/biophys/S1S3Dialog.java
@@ -18,6 +18,7 @@ package hu.unideb.med.biophys;
 
 import ij.IJ;
 import ij.ImagePlus;
+import ij.ImageStack;
 import ij.WindowManager;
 import ij.process.FloatProcessor;
 import ij.process.ImageConverter;
@@ -51,6 +52,9 @@ public class S1S3Dialog extends JDialog implements ActionListener {
     private ImagePlus donorImg;
     private ImagePlus transferImg;
     private ImagePlus acceptorImg;
+    private ImageStack donorImgSave = null;
+    private ImageStack transferImgSave = null;
+    private ImageStack acceptorImgSave = null;
     private JPanel panel;
     private JButton setDonorButton;
     private JButton setTransferButton;
@@ -62,6 +66,9 @@ public class S1S3Dialog extends JDialog implements ActionListener {
     private JButton setDonorThresholdButton;
     private JButton setTransferThresholdButton;
     private JButton setAcceptorThresholdButton;
+    private JButton resetDonorThresholdButton;
+    private JButton resetTransferThresholdButton;
+    private JButton resetAcceptorThresholdButton;
     private JButton calculateButton;
     private JButton setButton;
     private JButton resetButton;
@@ -171,24 +178,51 @@ public class S1S3Dialog extends JDialog implements ActionListener {
         autoflAInAField = new JTextField("0", 5);
         autoflAInAField.setToolTipText("<html><b>Correction for autofluorescence</b><br>If this value is set, it will be subtracted from each pixel along with<br>the background. Average autofluorescence can be calculated by<br>choosing <i>Image ▶ Calculate Autofluorescence...</i><html>");
         panel.add(autoflAInAField, gc);
+        gc.gridwidth = 2;
         gc.gridx = 0;
         gc.gridy = 9;
         setDonorThresholdButton = new JButton("Set threshold for donor channel");
         setDonorThresholdButton.addActionListener(this);
         setDonorThresholdButton.setActionCommand("setS1S3DonorThreshold");
         panel.add(setDonorThresholdButton, gc);
+        gc.gridwidth = GridBagConstraints.REMAINDER;
+        gc.gridx = 2;
+        gc.gridy = 9;
+        resetDonorThresholdButton = new JButton("Reset");
+        resetDonorThresholdButton.setToolTipText("Resets threshold settings for donor channel");
+        resetDonorThresholdButton.addActionListener(this);
+        resetDonorThresholdButton.setActionCommand("resetS1S3DonorThreshold");
+        panel.add(resetDonorThresholdButton, gc);
+        gc.gridwidth = 2;
         gc.gridx = 0;
         gc.gridy = 10;
         setTransferThresholdButton = new JButton("Set threshold for transfer channel");
         setTransferThresholdButton.addActionListener(this);
         setTransferThresholdButton.setActionCommand("setS1S3TransferThreshold");
         panel.add(setTransferThresholdButton, gc);
+        gc.gridwidth = GridBagConstraints.REMAINDER;
+        gc.gridx = 2;
+        gc.gridy = 10;
+        resetTransferThresholdButton = new JButton("Reset");
+        resetTransferThresholdButton.setToolTipText("Resets threshold settings for transfer channel");
+        resetTransferThresholdButton.addActionListener(this);
+        resetTransferThresholdButton.setActionCommand("resetS1S3TransferThreshold");
+        panel.add(resetTransferThresholdButton, gc);
+        gc.gridwidth = 2;
         gc.gridx = 0;
         gc.gridy = 11;
         setAcceptorThresholdButton = new JButton("Set threshold for acceptor channel");
         setAcceptorThresholdButton.addActionListener(this);
         setAcceptorThresholdButton.setActionCommand("setS1S3AcceptorThreshold");
         panel.add(setAcceptorThresholdButton, gc);
+        gc.gridwidth = GridBagConstraints.REMAINDER;
+        gc.gridx = 2;
+        gc.gridy = 11;
+        resetAcceptorThresholdButton = new JButton("Reset");
+        resetAcceptorThresholdButton.setToolTipText("Resets threshold settings for acceptor channel");
+        resetAcceptorThresholdButton.addActionListener(this);
+        resetAcceptorThresholdButton.setActionCommand("resetS1S3AcceptorThreshold");
+        panel.add(resetAcceptorThresholdButton, gc);
         gc.gridx = 0;
         gc.gridy = 12;
         JPanel radioPanel = new JPanel();
@@ -388,6 +422,7 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                         ImageProcessor ipD = donorImg.getProcessor();
                         int width = donorImg.getWidth();
                         int height = donorImg.getHeight();
+                        donorImgSave = new ImageStack(ipD.getWidth(), ipD.getHeight());
                         for (int x = 0; x < width; x++) {
                             for (int y = 0; y < height; y++) {
                                 float value = ipD.getPixelValue(x, y);
@@ -395,7 +430,11 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                                 ipD.putPixelValue(x, y, value);
                             }
                         }
+                        FloatProcessor flp = new FloatProcessor(ipD.getWidth(), ipD.getHeight());
+                        flp.setPixels(1, (FloatProcessor) ipD.duplicate());
+                        donorImgSave.addSlice("" + 1, flp);
                         donorImg.updateAndDraw();
+                        donorImgSave.setColorModel(ipD.getColorModel());
                         mainWindow.log("Subtracted autofluorescence " + "(" + df.format(autofl) + ") of donor channel. (S1/S3 calc.)");
                         subtractDonorButton.setBackground(mainWindow.greenColor);
                         subtractDonorButton.setOpaque(true);
@@ -409,6 +448,7 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                         ImageProcessor ipD = donorImg.getProcessor();
                         int width = donorImg.getWidth();
                         int height = donorImg.getHeight();
+                        donorImgSave = new ImageStack(ipD.getWidth(), ipD.getHeight());
                         double sum = 0;
                         int count = 0;
                         for (int i = 0; i < width; i++) {
@@ -428,8 +468,12 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                                 ipD.putPixelValue(x, y, value);
                             }
                         }
+                        FloatProcessor flp = new FloatProcessor(ipD.getWidth(), ipD.getHeight());
+                        flp.setPixels(1, (FloatProcessor) ipD.duplicate());
+                        donorImgSave.addSlice("" + 1, flp);
                         donorImg.updateAndDraw();
                         donorImg.killRoi();
+                        donorImgSave.setColorModel(ipD.getColorModel());
                         mainWindow.log("Subtracted background " + (autofl > 0 ? "and autofluorescence " : "") + "(" + df.format(backgroundAvgD) + ") of donor channel. (S1/S3 calc.)");
                         subtractDonorButton.setBackground(mainWindow.greenColor);
                         subtractDonorButton.setOpaque(true);
@@ -453,6 +497,7 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                         ImageProcessor ipT = transferImg.getProcessor();
                         int width = transferImg.getWidth();
                         int height = transferImg.getHeight();
+                        transferImgSave = new ImageStack(ipT.getWidth(), ipT.getHeight());
                         for (int x = 0; x < width; x++) {
                             for (int y = 0; y < height; y++) {
                                 float value = ipT.getPixelValue(x, y);
@@ -460,7 +505,11 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                                 ipT.putPixelValue(x, y, value);
                             }
                         }
+                        FloatProcessor flp = new FloatProcessor(ipT.getWidth(), ipT.getHeight());
+                        flp.setPixels(1, (FloatProcessor) ipT.duplicate());
+                        transferImgSave.addSlice("" + 1, flp);
                         transferImg.updateAndDraw();
+                        transferImgSave.setColorModel(ipT.getColorModel());
                         mainWindow.log("Subtracted autofluorescence " + "(" + df.format(autofl) + ") of transfer channel. (S1/S3 calc.)");
                         subtractTransferButton.setBackground(mainWindow.greenColor);
                         subtractTransferButton.setOpaque(true);
@@ -474,6 +523,7 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                         ImageProcessor ipT = transferImg.getProcessor();
                         int width = transferImg.getWidth();
                         int height = transferImg.getHeight();
+                        transferImgSave = new ImageStack(ipT.getWidth(), ipT.getHeight());
                         double sum = 0;
                         int count = 0;
                         for (int i = 0; i < width; i++) {
@@ -493,8 +543,12 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                                 ipT.putPixelValue(x, y, value);
                             }
                         }
+                        FloatProcessor flp = new FloatProcessor(ipT.getWidth(), ipT.getHeight());
+                        flp.setPixels(1, (FloatProcessor) ipT.duplicate());
+                        transferImgSave.addSlice("" + 1, flp);
                         transferImg.updateAndDraw();
                         transferImg.killRoi();
+                        transferImgSave.setColorModel(ipT.getColorModel());
                         mainWindow.log("Subtracted background " + (autofl > 0 ? "and autofluorescence " : "") + "(" + df.format(backgroundAvgT) + ") of transfer channel. (S1/S3 calc.)");
                         subtractTransferButton.setBackground(mainWindow.greenColor);
                         subtractTransferButton.setOpaque(true);
@@ -518,6 +572,7 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                         ImageProcessor ipA = acceptorImg.getProcessor();
                         int width = acceptorImg.getWidth();
                         int height = acceptorImg.getHeight();
+                        acceptorImgSave = new ImageStack(ipA.getWidth(), ipA.getHeight());
                         for (int x = 0; x < width; x++) {
                             for (int y = 0; y < height; y++) {
                                 float value = ipA.getPixelValue(x, y);
@@ -525,7 +580,11 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                                 ipA.putPixelValue(x, y, value);
                             }
                         }
+                        FloatProcessor flp = new FloatProcessor(ipA.getWidth(), ipA.getHeight());
+                        flp.setPixels(1, (FloatProcessor) ipA.duplicate());
+                        acceptorImgSave.addSlice("" + 1, flp);
                         acceptorImg.updateAndDraw();
+                        acceptorImgSave.setColorModel(ipA.getColorModel());
                         mainWindow.log("Subtracted autofluorescence " + "(" + df.format(autofl) + ") of acceptor channel. (S1/S3 calc.)");
                         subtractAcceptorButton.setBackground(mainWindow.greenColor);
                         subtractAcceptorButton.setOpaque(true);
@@ -539,6 +598,7 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                         ImageProcessor ipA = acceptorImg.getProcessor();
                         int width = acceptorImg.getWidth();
                         int height = acceptorImg.getHeight();
+                        acceptorImgSave = new ImageStack(ipA.getWidth(), ipA.getHeight());
                         double sum = 0;
                         int count = 0;
                         for (int i = 0; i < width; i++) {
@@ -558,8 +618,12 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                                 ipA.putPixelValue(x, y, value);
                             }
                         }
+                        FloatProcessor flp = new FloatProcessor(ipA.getWidth(), ipA.getHeight());
+                        flp.setPixels(1, (FloatProcessor) ipA.duplicate());
+                        acceptorImgSave.addSlice("" + 1, flp);
                         acceptorImg.updateAndDraw();
                         acceptorImg.killRoi();
+                        acceptorImgSave.setColorModel(ipA.getColorModel());
                         mainWindow.log("Subtracted background " + (autofl > 0 ? "and autofluorescence " : "") + "(" + df.format(backgroundAvgA) + ") of acceptor channel. (S1/S3 calc.)");
                         subtractAcceptorButton.setBackground(mainWindow.greenColor);
                         subtractAcceptorButton.setOpaque(true);
@@ -600,6 +664,69 @@ public class S1S3Dialog extends JDialog implements ActionListener {
                     setAcceptorThresholdButton.setOpaque(true);
                     setAcceptorThresholdButton.setBorderPainted(false);
                     break;
+                case "resetS1S3DonorThreshold": {
+                    if (donorImg == null) {
+                        mainWindow.logError("No image is set as donor channel. (S1/S3 calc.)");
+                        return;
+                    }
+                    if (donorImgSave == null) {
+                        mainWindow.logError("No saved image. (S1/S3 calc.)");
+                        return;
+                    }
+                    ImageStack newStack = new ImageStack(donorImgSave.getWidth(), donorImgSave.getHeight());
+                    FloatProcessor flp = new FloatProcessor(donorImgSave.getProcessor(1).getWidth(), donorImgSave.getProcessor(1).getHeight());
+                    flp.setPixels(1, (FloatProcessor) donorImgSave.getProcessor(1).duplicate());
+                    newStack.addSlice("" + 1, flp);
+                    donorImg.setStack(donorImg.getTitle(), newStack);
+                    donorImg.getProcessor().setColorModel(donorImgSave.getColorModel());
+                    donorImg.updateAndDraw();
+                    setDonorThresholdButton.setBackground(mainWindow.originalButtonColor);
+                    setDonorThresholdButton.setOpaque(false);
+                    setDonorThresholdButton.setBorderPainted(true);
+                    break;
+                }
+                case "resetS1S3TransferThreshold": {
+                    if (transferImg == null) {
+                        mainWindow.logError("No image is set as transfer channel. (S1/S3 calc.)");
+                        return;
+                    }
+                    if (transferImgSave == null) {
+                        mainWindow.logError("No saved image. (S1/S3 calc.)");
+                        return;
+                    }
+                    ImageStack newStack = new ImageStack(transferImgSave.getWidth(), transferImgSave.getHeight());
+                    FloatProcessor flp = new FloatProcessor(transferImgSave.getProcessor(1).getWidth(), transferImgSave.getProcessor(1).getHeight());
+                    flp.setPixels(1, (FloatProcessor) transferImgSave.getProcessor(1).duplicate());
+                    newStack.addSlice("" + 1, flp);
+                    transferImg.setStack(transferImg.getTitle(), newStack);
+                    transferImg.getProcessor().setColorModel(transferImgSave.getColorModel());
+                    transferImg.updateAndDraw();
+                    setTransferThresholdButton.setBackground(mainWindow.originalButtonColor);
+                    setTransferThresholdButton.setOpaque(false);
+                    setTransferThresholdButton.setBorderPainted(true);
+                    break;
+                }
+                case "resetS1S3AcceptorThreshold": {
+                    if (acceptorImg == null) {
+                        mainWindow.logError("No image is set as acceptor channel. (S1/S3 calc.)");
+                        return;
+                    }
+                    if (acceptorImgSave == null) {
+                        mainWindow.logError("No saved image. (S1/S3 calc.)");
+                        return;
+                    }
+                    ImageStack newStack = new ImageStack(acceptorImgSave.getWidth(), acceptorImgSave.getHeight());
+                    FloatProcessor flp = new FloatProcessor(acceptorImgSave.getProcessor(1).getWidth(), acceptorImgSave.getProcessor(1).getHeight());
+                    flp.setPixels(1, (FloatProcessor) acceptorImgSave.getProcessor(1).duplicate());
+                    newStack.addSlice("" + 1, flp);
+                    acceptorImg.setStack(acceptorImg.getTitle(), newStack);
+                    acceptorImg.getProcessor().setColorModel(acceptorImgSave.getColorModel());
+                    acceptorImg.updateAndDraw();
+                    setAcceptorThresholdButton.setBackground(mainWindow.originalButtonColor);
+                    setAcceptorThresholdButton.setOpaque(false);
+                    setAcceptorThresholdButton.setBorderPainted(true);
+                    break;
+                }
                 case "calculate":
                     if (donorImg == null) {
                         mainWindow.logError("No image is set as donor channel. (S1/S3 calc.)");
