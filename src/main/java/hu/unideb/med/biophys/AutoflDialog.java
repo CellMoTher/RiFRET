3dcd0a20c87f60ed2e99584cf5e04a8b1fb0d2e9
Cameron Lloyd <lloyd@med.unideb.hu>
2020-01-07 22:19:01 +0100

    Update license headers

--- src/main/java/hu/unideb/med/biophys/AutoflDialog.java
+++ src/main/java/hu/unideb/med/biophys/AutoflDialog.java
@@ -1,18 +1,23 @@
-/*
- * Copyright (C) 2009 - 2019 RiFRET developers.
- *
+/*-
+ * #%L
+ * an ImageJ plugin for intensity-based three-filter set (ratiometric) FRET.
+ * %%
+ * Copyright (C) 2009 - 2020 RiFRET developers.
+ * %%
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
  */
 package hu.unideb.med.biophys;
 
