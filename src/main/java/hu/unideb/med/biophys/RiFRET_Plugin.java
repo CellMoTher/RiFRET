bda208975a8de1bc0b99ee9cf31b89e632dfaf42
Cameron Lloyd <lloyd@med.unideb.hu>
2023-02-19 16:06:25 +0000

    Update license headers

--- src/main/java/hu/unideb/med/biophys/RiFRET_Plugin.java
+++ src/main/java/hu/unideb/med/biophys/RiFRET_Plugin.java
@@ -1,19 +1,19 @@
 /*-
  * #%L
- * an ImageJ plugin for intensity-based three-filter set (ratiometric) FRET.
+ * an ImageJ plugin for intensity-based three-filter set FRET.
  * %%
- * Copyright (C) 2009 - 2021 RiFRET developers.
+ * Copyright (C) 2009 - 2023 RiFRET developers.
  * %%
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
